d5fc5ed5e6e8424e5ec03f93d10364d46aee1c21
jinmyeongAN <jinmyeong.an@gmail.com>
2023-06-14 16:06:14 +0900

    feat: change 5-fold to 1-fold

--- get_data_id.py
+++ get_data_id.py
@@ -74,23 +74,23 @@ random.shuffle(eid_list)
 
 # fold 나누기 -> 총 5개의 fold로 나누기
 len_eid_list = len(eid_list)
-fold_unit = int(len_eid_list / 6)
+fold_unit = int(len_eid_list / 10)
 
 val_eid_list = eid_list[:fold_unit]
 
-fold_0 = eid_list[fold_unit:fold_unit * 2]
-fold_1 = eid_list[fold_unit * 2:fold_unit * 3]
-fold_2 = eid_list[fold_unit * 3:fold_unit * 4]
-fold_3 = eid_list[fold_unit * 4:fold_unit * 5]
-fold_4 = eid_list[fold_unit * 5:]
+fold_0 = eid_list[fold_unit:]
+# fold_1 = eid_list[fold_unit * 2:fold_unit * 3]
+# fold_2 = eid_list[fold_unit * 3:fold_unit * 4]
+# fold_3 = eid_list[fold_unit * 4:fold_unit * 5]
+# fold_4 = eid_list[fold_unit * 5:]
 
 fold_0_test, fold_0_train = get_test_train(fold_0)
-fold_1_test, fold_1_train = get_test_train(fold_1)
-fold_2_test, fold_2_train = get_test_train(fold_2)
-fold_3_test, fold_3_train = get_test_train(fold_3)
-fold_4_test, fold_4_train = get_test_train(fold_4)
+# fold_1_test, fold_1_train = get_test_train(fold_1)
+# fold_2_test, fold_2_train = get_test_train(fold_2)
+# fold_3_test, fold_3_train = get_test_train(fold_3)
+# fold_4_test, fold_4_train = get_test_train(fold_4)
 
-data_ids = {"val": val_eid_list, "fold0": {'test': fold_0_test, 'train': fold_0_train}, "fold1": {'test': fold_1_test, 'train': fold_1_train}, "fold2": {
-    'test': fold_2_test, 'train': fold_2_train}, "fold3": {'test': fold_3_test, 'train': fold_3_train}, "fold4": {'test': fold_4_test, 'train': fold_4_train}, }
+data_ids = {"val": val_eid_list, "fold0": {
+    'test': fold_0_test, 'train': fold_0_train}, }
 
 save_pickle(data=data_ids, path=PHEME_path, filename='PAN_data_ids')
